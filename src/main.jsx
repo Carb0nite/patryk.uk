46217ebb5b4bd8bb9f0d39ea6e58bc26caf8c540
Patryk Sobczak <pat.sobczak@gmail.com>
2026-03-01 14:43:33 +0000

    add finances page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import SankeyApp from './components/SankeyApp.jsx'
+
+const path = window.location.pathname;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {path === '/fire' ? <SankeyApp /> : <App />}
   </StrictMode>,
 )
